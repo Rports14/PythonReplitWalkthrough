633e272e6a257e87720c63f7efce9f4fd6c961f3
RobertP14 <117120178+Rports14@users.noreply.github.com>
2022-12-21 20:08:03 +0000

    some random Mumbo Jumbo

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from replit import db
+import os
+
+# store my name in the replit db
+#db['name'] = "Robert"
+
 def factorial(n):
   if n == 0:
     return 1
@@ -5,3 +11,15 @@ def factorial(n):
     return n * factorial(n - 1)
     
 print(factorial(5))
+
+print(db['name'])
+
+keys = db.keys()
+
+print(keys)
+
+for key in db.keys():
+  print(db[key])
+
+myApiKey = os.environ['apiKey']
+print(myApiKey)
